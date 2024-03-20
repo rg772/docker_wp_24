4cbc856a1cf28e1f63a2d0ae2148150d0f3a05ff
7kxRQ9yfN/yQ2Qgh <username@users.noreply.github.com>
2024-03-20 11:13:08 -0500

    trimming down

--- build_wp_and_run.sh
+++ build_wp_and_run.sh
@@ -6,8 +6,6 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-# Run docker system prune -a
-# docker system prune -a -f
 
 # Build the Docker image
 docker build -t $DOCKER_IMAGE_NAME .
@@ -15,10 +13,4 @@ docker build -t $DOCKER_IMAGE_NAME .
 # Run the container locally
 docker run -d -p 8080:80 --name $CONTAINER_NAME $DOCKER_IMAGE_NAME
 
-# Add WP-CLI to the container
-#docker exec -it $CONTAINER_NAME bash -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp"
-  
-# CLI... reference
-echo To get SSH in
-echo docker exec -it $CONTAINER_NAME /bin/bash    
 
